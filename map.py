d9cea1f6d8f36e9ac4cd96bc034c9d8d1801ffe8
Anne Archibald <peridot.faceted@gmail.com>
2012-04-25 17:12:53 -0400

    Set up yaml serialization for maps
    Included the pure-python part of the pyyaml source code, with one slight modification, namely to prefer block representation for multiline string literals (in particular, the map and seen arrays).

--- map.py
+++ map.py
@@ -20,10 +20,27 @@
 #       
 #       
 
+import unicodedata
+
 import numpy as np
+
+import yaml
 import terrain
 
-class Map(object):
+def unicode_usable(c):
+    if c in '\\ \t':
+        return False
+    if not unicodedata.category(c) in [
+            'Ll', 'Lt', 'Lu', 'Me', 'Nd', 'Nl', 'No', 
+            'Pc', 'Pd', 'Pe', 'Pf', 'Pi', 'Po', 'Ps',
+            'Sc', 'Sm']:
+        return False
+    return True
+usable_unicode_chars = [unichr(i) for i in range(65536) if unicode_usable(unichr(i))]
+
+class Map(yaml.YAMLObject):
+    yaml_tag = "!Map"
+    
     def __init__(self, size):
         self.size = size
         self.w, self.h = self.size
@@ -52,6 +69,42 @@ class Map(object):
         return ("Map {0} by {1}:\n".format(self.w, self.h) + 
             "\n".join("".join(self.terrain_types[v].roguechar for v in l) 
                         for l in self.map.T))
+    def __getstate__(self):
+        d = self.__dict__.copy()
+        del d['terrain_types_reverse']
+        del d['size']
+        ttmap = {}
+        ttchar = [' ']
+        j = 0
+        for t in self.terrain_types[1:]:
+            c = t.roguechar
+            while c in ttmap:
+                c = usable_unicode[j]
+                j += 1
+            ttmap[c] = t
+            ttchar.append(c)
+        d['map'] = '\n'.join("".join(ttchar[self.map[i,j]] for i in range(self.w)) for j in range(self.h))+"\n"
+        d['seen'] = '\n'.join("".join('*' if self.seen[i,j] else '.' for i in range(self.w)) for j in range(self.h))+"\n"
+        d['terrain_types'] = ttmap
+        return d
+
+    def __setstate__(self, d):
+        self.__dict__ = d
+        self.size = (self.w, self.h)
+        ttmap = self.terrain_types
+        map = self.map
+        seen = self.seen
+        self.map = np.zeros(self.size, np.uint8)
+        self.seen = np.zeros(self.size, np.uint8)
+        self.terrain_types = [terrain.void]
+        self.terrain_types_reverse = { self.terrain_types[0]: 0 }
+        for j,l in enumerate(map.split("\n")):
+            for i,c in enumerate(l):
+                self.set_terrain((i,j),ttmap[c])
+        for j,l in enumerate(seen.split("\n")):
+            for i,c in enumerate(l):
+                if c!='.':
+                    self.seen[i,j] = 1
 
 def load_ascii_map(f):
     a = open(f,"rt").readlines()
@@ -75,4 +128,6 @@ def load_ascii_map(f):
     return M
 
 if __name__ == '__main__':
-    print load_ascii_map("data/testmap1.txt")
+    M = load_ascii_map("data/testmap1.txt")
+    print yaml.dump(M)
+    print yaml.load(yaml.dump(M))
