e1e7129ecc937a15a12747b9cf8e61b4251c8a36
Anne Archibald <peridot.faceted@gmail.com>
2012-07-23 07:33:12 -0400

    Adding some missing files

---
+++ aibrain.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#       
+#       Copyright 2012 Anne Archibald <peridot.faceted@gmail.com>
+#       
+#       This program is free software; you can redistribute it and/or modify
+#       it under the terms of the GNU General Public License as published by
+#       the Free Software Foundation; either version 2 of the License, or
+#       (at your option) any later version.
+#       
+#       This program is distributed in the hope that it will be useful,
+#       but WITHOUT ANY WARRANTY; without even the implied warranty of
+#       MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#       GNU General Public License for more details.
+#       
+#       You should have received a copy of the GNU General Public License
+#       along with this program; if not, write to the Free Software
+#       Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+#       MA 02110-1301, USA.
+#       
+# 
+
+import random
+
+import action
+
+class Actor(object):
+    
+    def next(self):
+        return None
+
+class Wanderer(Actor):
+    def next(self):
+        return random.choice([action.Advance(), action.Turn(1), action.Turn(0)])
+
+
+if __name__=='__main__':
+    pass
